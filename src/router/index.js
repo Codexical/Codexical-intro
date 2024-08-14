44058bfeca2a865377eba22771a1bf1fe0f67229
Aura <aura.cs12@nycu.edu.tw>
2024-08-14 20:52:18 +0800

    feat: add router

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Codexical",
+    component: () => import("../views/mainView.vue"),
+    meta: {
+      title: "Codexical",
+    },
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "404",
+    component: () => import("../views/notFound.vue"),
+    meta: {
+      title: "404",
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title;
+  next();
+});
+
+export default router;
