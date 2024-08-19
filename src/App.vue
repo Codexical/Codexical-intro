35280507ceb6167932c74e4a383eeb66dd84a1a2
CC.Tsai <aura.cs12@nycu.edu.tw>
2024-08-19 20:59:32 +0800

    Feat/appbar and footer (#1)
    * feat(App.vue): add appbar and footer to app
    
    * feat(AppBar.vue): create appbar
    
    * feat(FooterView.vue): create footer
    
    * fix(AppBar.vue): change padding from em to vh
    
    * fix(App.vue): change margin-top from em to vh
    
    * feat(AppBar.vue): add logo as button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <v-app>
-    <v-main>
+    <AppBar />
+    <v-main style="margin-top: 6vh;">
       <router-view />
     </v-main>
+    <FooterView />
   </v-app>
 </template>
+
+<script>
+import AppBar from './components/AppBar.vue'
+import FooterView from './components/FooterView.vue'
+export default {
+  components: {
+    AppBar,
+    FooterView
+  }
+}
+</script>
---
+++ src/components/FooterView.vue
@@ -0,0 +1,45 @@
+<template>
+    <footer class="footer">
+        <v-row class="options">
+            <v-btn v-for="link in linkOption" :key="link.name" variant="text" :href="link.url">{{ link.name }}</v-btn>
+        </v-row>
+        <div class="copyright">
+            &copy; 2024 Codexical. All rights reserved.
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        linkOption: [
+            { name: 'Home', url: '#' },
+            { name: 'About Us', url: '#' },
+            { name: 'Services', url: '#' },
+            { name: 'Contact Us', url: '#' }
+        ]
+    })
+}</script>
+
+<style scoped>
+.footer {
+    background-color: #f5f5f5;
+    padding: 1rem 0;
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.options {
+    padding: 1rem 0;
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: center;
+}
+
+
+.copyright {
+    font-size: 0.8rem;
+}
+</style>
