74f30522cebbacbde3650257b3ddad297ec663e9
hanasay <hanasay16@gmail.com>
2024-08-22 15:32:11 +0800

    feat(/src/views/contactUs.vue): Change email send method to local

--- src/views/contactUs.vue
+++ src/views/contactUs.vue
@@ -1,54 +1,53 @@
 <template>
     <div class="contact-us">
-      <h1>聯絡我們</h1>
-      <form @submit.prevent="handleSubmit">
-        <div>
-          <label for="name">姓名:</label>
-          <input type="text" v-model="form.name" id="name" required>
-        </div>
-        <div>
-          <label for="email">電子郵件:</label>
-          <input type="email" v-model="form.email" id="email" required>
-        </div>
-        <div>
-          <label for="message">訊息:</label>
-          <textarea v-model="form.message" id="message" required></textarea>
-        </div>
+      <h1 v-motion :initial="{ opacity: 0, x: -100}" :enter="{ opacity: 1, x: 0}" :delay="0" :duration="1000">
+        聯絡我們
+      </h1>
+      &nbsp;
+      <h2 v-motion :initial="{ opacity: 0, x: -100}" :enter="{ opacity: 1, x: 0}" :delay="0" :duration="1000">
+        期待與您的合作
+        <br>
+        如果您對於我們的服務感興趣
+        <br> 
+        可以透過以下的方式與我們取得聯繫
+      </h2>
+      <form v-motion :initial="{ opacity: 0, x: -100}" :enter="{ opacity: 1, x: 0}" :delay="200" :duration="1000" @submit.prevent="handleSubmit">
+          <div >
+            <label for="message">Email</label>
+            <textarea v-model="message" id="message" required></textarea>
+          </div>
         <button type="submit">送出</button>
       </form>
+
+      &nbsp;
+      <div v-motion :initial="{ opacity: 0, x: -100}" :enter="{ opacity: 1, x: 0}" :delay="200" :duration="1000">
+        <h1>
+          其他聯絡方式
+        </h1>
+        <span class="mdi mdi-discord mdi-48px"></span>
+      </div>
     </div>
   </template>
-  
 <script>
-import axios from 'axios';
 export default {
   data() {
     return {
-      form: {
-        name: '',
-        email: '',
-        message: ''
-      }
+      message: ''
     };
   },
   methods: {
     async handleSubmit() {
-      try {
-        const response = await axios.post('/api/contact', this.form);
-        console.log('表單已成功提交', response.data);
-      } catch (error) {
-        console.error('提交表單時出錯', error);
-      }
+      window.open(`mailto:abc@example.com?subject=合作邀約&body=${this.message}`);
     }
   }
 };
-
 </script>
 
 <style scoped>
 .contact-us {
     max-width: 600px;
     margin: 0 auto;
+    text-align: center;
 }
 
 label {
